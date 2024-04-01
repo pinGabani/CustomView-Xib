d1a9fa66f44841c85858beff3225e41f2fff13f5
Pinali <pinali.gabani@gmail.com>
2024-04-01 11:52:24 +0530

    Initial Commit

---
+++ CustomView+Xib/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomView+Xib
+//
+//  Created by pinali gabani on 01/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
